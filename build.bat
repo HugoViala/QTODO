24060afd334ce64e124825c21504c0ea5b042c4e
HugoViala <hugo.viala@gmail.com>
2015-04-30 23:27:23 +0200

    Having fun with Qt

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set IncludeFlags=-I C:\Qt5.4.1\5.4\msvc2013_64\include\QtWidgets\ -I C:\Qt5.4.1\5.4\msvc2013_64\include\
+set IncludeFlags=-I C:\Qt5.4.1\5.4\msvc2013_64\include\ -I C:\Qt5.4.1\5.4\msvc2013_64\include\QtWidgets\ -I C:\Qt5.4.1\5.4\msvc2013_64\include\QtCore\
+
 set LinkerFlags=C:\Qt5.4.1\5.4\msvc2013_64\lib\Qt5Widgets.lib C:\Qt5.4.1\5.4\msvc2013_64\lib\Qt5Core.lib
 
 cl %IncludeFlags% qtodo.cpp %LinkerFlags%
--- qtodo.cpp
+++ qtodo.cpp
@@ -1,13 +1,35 @@
+#include <QObject>
 #include <QApplication>
 #include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
 
 int
 main(int argc, char** argv)
 {
     QApplication app(argc, argv);
-    QPushButton myButton("Hello button", 0);
-    myButton.resize(300,90);
+    QWidget window;
+    window.setWindowTitle("Test window title");
+    window.resize(300,90);
 
-    myButton.show();
+    QPushButton quitButton("Quit", &window);
+    QLabel testLabel(&window);
+    testLabel.setText("AAAAAAAAA");
+    testLabel.setFrameRect(QRect(0,0, 10, 30));
+    testLabel.setFrameStyle(QFrame::Panel);
+    testLabel.setLineWidth(2);
+    testLabel.move(50,15);
+    quitButton.move(200,15);
+    //quitButton.resize(300,90);
+
+    QLineEdit line("Type text", &window);
+    line.move(50,60);
+    
+    window.show();
+
+
+    QObject::connect(&line, SIGNAL(textChanged(QString)), &testLabel, SLOT(setText(QString)));
+    QObject::connect(&quitButton, &QPushButton::clicked, &app, &QApplication::quit);
+    
     return(app.exec());
 }
