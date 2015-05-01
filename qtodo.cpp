e8ec112b7f079acf6400a19eeef9ac6d3ed3273a
HugoViala <hugo.viala@gmail.com>
2015-05-02 00:35:50 +0200

    Back to square one
    To build on strong bases.

--- qtodo.cpp
+++ qtodo.cpp
@@ -13,44 +13,9 @@ main(int argc, char** argv)
 
     // Creating Window
     QWidget window;
-    window.setWindowTitle("Test window title");
-    window.resize(300,90);
+    window.setWindowTitle("QTODO");
+    window.resize(400,500);
     
-    // vertLayout is the main layout of the window
-    QBoxLayout vertLayout(QBoxLayout::TopToBottom, &window);
-
-    // top groupBox to contain the label and the quit button
-    QGroupBox groupBox("Horizontal", &window);
-    
-    
-    // boxLayout is the layout of groupBox
-    QBoxLayout boxLayout(QBoxLayout::LeftToRight, &groupBox);  
-
-    // The label to be displayed
-    QLabel testLabel(&window);
-    testLabel.setFrameRect(QRect(0,0, 10, 30));
-    testLabel.setFrameStyle(QFrame::Panel);
-    testLabel.setLineWidth(2);
-    
-    // A quit button
-    QPushButton quitButton("Quit", &window);
-    quitButton.move(200,15);
-
-    // Adding the widgets to the groupbox layout
-    boxLayout.addWidget(&testLabel);
-    boxLayout.addWidget(&quitButton);
-    
-    QLineEdit line("Type text", &window);
-    vertLayout.addWidget(&groupBox);
-    vertLayout.addWidget(&line);
-
-    
-    QObject::connect(&line, &QLineEdit::textChanged,
-                     &testLabel, &QLabel::setText);
-    QObject::connect(&quitButton, &QPushButton::clicked,
-                     &app, &QApplication::quit);
-
-    window.setLayout(&vertLayout);
     window.show();
     
     return(app.exec());
