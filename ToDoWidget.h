ff8fb4d9acf597a015f4ded1ec5bfe6fb73654b1
HugoViala <hugo.viala@gmail.com>
2015-05-03 18:57:30 +0200

    Getting rid of undesirable header

--- ToDoWidget.h
+++ ToDoWidget.h
@@ -1,11 +1,4 @@
 #if !defined(TODOWIDGET_H)
-/* ========================================================================
-   $File: $
-   $Date: $
-   $Revision: $
-   $Creator: Casey Muratori $
-   $Notice: (C) Copyright 2014 by Molly Rocket, Inc. All Rights Reserved. $
-   ======================================================================== */
 
 #define TODOWIDGET_H
 #include <QGroupBox>
